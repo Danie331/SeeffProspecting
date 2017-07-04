2238c333790c588a60c408f8f3f6e3fd1a7bca89
danie.vdm <danie.vdm@seeff.com>
2017-07-04 18:31:07 +0200

    TaskScheduler: adusted hours for suburb maintenance.

--- Source/ProspectingTaskScheduler/Core/Spatial/SuburbMaintenance.cs
+++ Source/ProspectingTaskScheduler/Core/Spatial/SuburbMaintenance.cs
@@ -11,25 +11,29 @@ namespace ProspectingTaskScheduler.Core.Spatial
     {
         public static void ReindexSuburbsRequiringMaintenance()
         {
-            try {
-                using (var spatial = new seeff_spatialEntities())
+            if (DateTime.Now.Hour >= 2 && DateTime.Now.Hour < 6)
+            {
+                try
                 {
-                    string connStr = WebConfigurationManager.ConnectionStrings["seeff_spatial"].ConnectionString;
-                    using (var conn = new SqlConnection(connStr))
-                    using (var command = new SqlCommand("reindex_seeff_suburb", conn)
+                    using (var spatial = new seeff_spatialEntities())
                     {
-                        CommandType = System.Data.CommandType.StoredProcedure
-                    })
-                    {
-                        command.CommandTimeout = int.MaxValue;
-                        conn.Open();
-                        command.ExecuteNonQuery();
+                        string connStr = WebConfigurationManager.ConnectionStrings["seeff_spatial"].ConnectionString;
+                        using (var conn = new SqlConnection(connStr))
+                        using (var command = new SqlCommand("reindex_seeff_suburb", conn)
+                        {
+                            CommandType = System.Data.CommandType.StoredProcedure
+                        })
+                        {
+                            command.CommandTimeout = int.MaxValue;
+                            conn.Open();
+                            command.ExecuteNonQuery();
+                        }
                     }
                 }
-            }
-            catch (Exception e)
-            {
-                Utils.LogException(e);
+                catch (Exception e)
+                {
+                    Utils.LogException(e);
+                }
             }
         }
 
