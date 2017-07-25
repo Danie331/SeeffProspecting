95f9aae35c6c620d21978fd76152bd4f032856a9
Danie van der Merwe <danie.vdm@seeff.com>
2017-07-25 12:32:38 +0200

    Task Scheduler: updates to Followups notifier

--- Source/ProspectingTaskScheduler/Core/Notifications/ProspectingFollowup.cs
+++ Source/ProspectingTaskScheduler/Core/Notifications/ProspectingFollowup.cs
@@ -12,10 +12,10 @@ namespace ProspectingTaskScheduler.Core.Notifications
         public string Comment { get; set; }
         public string CreatedByUsername { get; set; }
         public string RelatedToContactPerson { get; set; }
-        public string PrimaryContactNo { get; set; }
-        public string PrimaryEmailAddress { get; set; }
-        public string PrimaryContactNoFormatted { get; set; }
-        public string PrimaryEmailAddressFormatted { get; set; }
+        //public string PrimaryContactNo { get; set; }
+        //public string PrimaryEmailAddress { get; set; }
+        //public string PrimaryContactNoFormatted { get; set; }
+        //public string PrimaryEmailAddressFormatted { get; set; }
         public string PropertyAddress { get; set; }
         public string FollowupActivityTypeName { get; set; }       
     }
